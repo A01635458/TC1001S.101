9f2d920471679f5c7254eeafc8fd7989426adfbf
A01635458 <A01635458@tec.mx>
2024-09-09 09:06:39 -0600

    Añadido color amarillo y completada la función de círculo

--- paint.py
+++ paint.py
@@ -36,9 +36,17 @@ def square(start, end):
     end_fill()
 
 
-def circle(start, end):
+def draw_circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    #codigo modificado usando nano por Luisa
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    radius = ((end.x - start.x)**2 + (end.y - start.y)**2) ** 0.5
+    circle(radius)
+    end_fill()
+	
 
 
 def rectangle(start, end):
@@ -79,9 +87,11 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+#color agregado por Luisa usando nano
+onkey(lambda: color('yellow'),'Y')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', draw_circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
